dbcd6fddef2d1820ffe6d331fbf70c8d0612d37f
IVAN <ivanbatur5g@gmail.com>
2020-11-15 18:55:06 +0100

    Reset App

--- firegram/src/App.js
+++ firegram/src/App.js
@@ -1,18 +1,13 @@
-import React, { useState } from "react"
+import React from "react"
 import Header from "./comps.js/Header"
-import ImageGrid from "./comps.js/ImageGrid"
-import UploadForm from "./comps.js/UploadForm"
-import Modal from "./comps.js/Modal"
 
 function App() {
   // const [file, setFile] = useState(null)
-  const [image, setImage] = useState(null)
+  // const [image, setImage] = useState(null)
   return (
     <div className="App">
       <Header />
-      <UploadForm />
-      <ImageGrid setImage={setImage} />
-      {image && <Modal image={image} setImage={setImage}/>}
+      
     </div>
   );
 }
